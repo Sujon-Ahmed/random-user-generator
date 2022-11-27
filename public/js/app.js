610551330be2bf72054107511ec6159d06bb9340
Sujon-Ahmed <sujonahmed5284@gmail.com>
2022-11-27 22:38:36 +0600

    remove siblings active class with meta_name ib

--- public/js/app.js
+++ public/js/app.js
@@ -23,53 +23,43 @@ $('#newBtn').click(function () {
 });
 var response;
 function sujon(data) {
-    // var info = data.results[0];
     response = data.results[0];
     $("#meta-name").text(response.name.first + " " + response.name.last);
     $("#user-avatar").attr("src", response.picture.large);
 }
 
 $('#user').mouseenter(function () {
-    $(this).addClass('active');
-    $('#envelope, #calender, #map-marker, #phone-alt, #lock').removeClass('active');
+    $(this).addClass('active').siblings().removeClass('active');
     $('#meta-content').text('My name is');
-    // $('#meta-name').text('Sujon Ahmed');
     $("#meta-name").text(response.name.first + " " + response.name.last);
 });
 
 $('#envelope').mouseenter(function () {
-    $(this).addClass('active');
-    $('#user, #calender, #map-marker, #phone-alt, #lock').removeClass('active');
+    $(this).addClass('active').siblings().removeClass('active');
     $('#meta-content').text('My email address is');
     $("#meta-name").text(response.email);
-    // console.log(response.email);
-    // $("#meta-name").text('sujonahmed5284@gmail.com');
 });
 
 $('#calender').mouseenter(function () {
-    $(this).addClass('active');
-    $('#user, #envelope, #map-marker, #phone-alt, #lock').removeClass('active');
+    $(this).addClass('active').siblings().removeClass('active');
     $('#meta-content').text('My birthday is');
     $("#meta-name").text(response.dob.date);
 });
 
 $('#map-marker').mouseenter(function () {
-    $(this).addClass('active');
-    $('#user, #envelope, #calender, #phone-alt, #lock').removeClass('active');
+    $(this).addClass('active').siblings().removeClass('active');
     $('#meta-content').text('My address is');
-    $("#meta-name").text(`${response.location.city}, ${response.location.state}, ${response.location.country}`);
+    $("#meta-name").text(`${response.location.city}, ${response.location.country}`);
 });
 
 $('#phone-alt').mouseenter(function () {
-    $(this).addClass('active');
-    $('#user, #envelope, #calender, #map-marker , #lock').removeClass('active');
+    $(this).addClass('active').siblings().removeClass('active');
     $('#meta-content').text('My phone number is');
     $("#meta-name").text(response.phone);
 });
 
 $('#lock').mouseenter(function () {
-    $(this).addClass('active');
-    $('#user, #envelope, #calender, #map-marker , #phone-alt').removeClass('active');
+    $(this).addClass('active').siblings().removeClass('active');
     $('#meta-content').text('My password is');
-    $("#meta-name").text(response.login.password);
+    $("#meta-name").text(response.login.md5);
 });
